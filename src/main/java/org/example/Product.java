4fc64b8bbfab3129b25a27924f271111e7fbb239
Vincenzo M <vincenzomaiorana@gmail.com>
2023-03-23 15:29:23 +0100

    Update Product.java

--- src/main/java/org/example/Product.java
+++ src/main/java/org/example/Product.java
@@ -1,4 +1,47 @@
 package org.example;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+
 public class Product {
+
+    @Getter
+    private long id;
+    public static long lastid = 0;
+    @Getter
+    private String name = "No Name";
+    @Getter
+    private String category = "Generic";
+    @Getter
+    @Setter
+    private double price = 0.00d;
+
+    public Product() {
+        this.id = Product.lastid++;
+    }
+
+    public Product(String name) {
+        this();
+        this.name = name;
+    }
+
+    public Product(String name, double price) {
+        this(name);
+        this.price = price;
+    }
+
+    public Product(String name, String cat, double price) {
+        this(name, price);
+        this.category = cat;
+    }
+
+    public void sconto(Product p, double scontopc) {
+        double temprice = p.getPrice();
+        double pricediscount = temprice - (temprice * scontopc);
+        p.setPrice(pricediscount);
+    }
+
+
 }
