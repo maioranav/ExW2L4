96aa197d5e66eac516fd32cfcadb6c8647a6312d
Vincenzo M <vincenzomaiorana@gmail.com>
2023-03-23 15:29:20 +0100

    Update Customer.java

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -1,4 +1,29 @@
 package org.example;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+
 public class Customer {
+
+    @Getter
+    private long id;
+    public static long lastid = 0;
+    @Getter
+    @Setter
+    private String name;
+    @Getter
+    @Setter
+    private int tier = 1;
+
+    public Customer(String name) {
+        this.id = Customer.lastid++;
+        this.name = name;
+    }
+
+    public Customer(String name, int tier) {
+        this(name);
+        this.tier = tier;
+    }
 }
