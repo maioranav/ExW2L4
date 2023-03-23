84bc5619a290502bb715b128e37864bf69725a45
Vincenzo M <vincenzomaiorana@gmail.com>
2023-03-23 15:29:39 +0100

    created main arrays

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Main {
+
+    public static ArrayList<Customer> customers = new ArrayList<>();
+    public static ArrayList<Product> products = new ArrayList<>();
+    public static ArrayList<Order> orders = new ArrayList<>();
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
     }
 }
